e60ecd62103aee4a6cda99e5e2d366d3962e78ee
juanbrau-or <149653585+juanbrau-or@users.noreply.github.com>
2023-11-15 23:04:00 -0600

    Update equipo.h

--- equipo.h
+++ equipo.h
@@ -1,42 +1,96 @@
-#include <string>
+#ifndef EQUIPO_H_
+#define Equipo_H_
 
-const int MAX = 100;
+#include <iostream>
+#include <set>
+#include <string>
+#include "empleado.h"
 
 class Equipo {
-    private:
-        std::string nombre;
-        int total_jugadores;
-    public:
-        Equipo();
-        Equipo(std::string nombre_ini, int jugadores_iniciales);
-        std::string get_nombre();
-        int get_numJugadores();
-        void set_nombre(std::string new_nombre);
-        void set_numJugadores(int new_jugadores);
+private:
+    std::string nombre;
+    int campeonatos;
+    std::set<Atleta> jugadores;
+    Entrenador entrenador;
+public:
+    Equipo():nombre(""),campeonatos(0),entrenador(Entrenador()){};
+    Equipo(std::string name, int champs):nombre(name),campeonatos(champs),entrenador(Entrenador()){};
+    Equipo(std::string name, int champs, Entrenador& trainer);
+    std::string getNombre();
+    int getCampeonatos();
+    std::set<Atleta> getJugadores();
+    Entrenador getEntrenador();
+    void imprimeJugadores();
+    void setNombre(std::string);
+    void aumentaCampeonatos();
+    void setCampeonatos(int);
+    void contrataAtleta(Atleta&);
+    void vendeAtleta(Atleta&);
+    void contrataEntrenador(Entrenador&);
+    void despideEntrenador(Entrenador&);
 };
 
-Equipo::Equipo() {
-    nombre = "";
-    total_jugadores = 0;
+Equipo::Equipo(std::string name, int champs, Entrenador& trainer) {
+    nombre = name;
+    campeonatos = champs;
+    entrenador = trainer;
+    trainer.cambiaEquipo(getNombre());
 }
 
-Equipo::Equipo(std::string nombre_ini, int jugadores_iniciales) {
-    nombre = nombre_ini;
-    total_jugadores = jugadores_iniciales;
-}
-
-std::string Equipo::get_nombre() {
+std::string Equipo::getNombre() {
     return nombre;
 }
 
-int Equipo::get_numJugadores() {
-    return total_jugadores;
+int Equipo::getCampeonatos() {
+    return campeonatos;
 }
 
-void Equipo::set_nombre(std::string new_nombre) {
-    nombre = new_nombre;
+std::set<Atleta> Equipo::getJugadores() {
+    return jugadores;
 }
 
-void Equipo::set_numJugadores(int new_jugadores) {
-    total_jugadores = new_jugadores;
+Entrenador Equipo::getEntrenador() {
+    return entrenador;
 }
+
+void Equipo::imprimeJugadores() {
+    std::cout << "Lista de jugadores: " << std::endl;
+    for( Atleta jug:jugadores ) {
+        std::cout << jug.getNombre() << std::endl;
+    }
+    std::cout << std::endl;
+}
+
+void Equipo::setNombre(std::string name) {
+    nombre = name;
+}
+
+void Equipo::aumentaCampeonatos() {
+    campeonatos++;
+}
+
+void Equipo::setCampeonatos(int champs) {
+    campeonatos = champs;
+}
+
+void Equipo::contrataAtleta(Atleta& jugador) {
+    jugadores.insert(jugador);
+    jugador.cambiaEquipo(getNombre());
+}
+
+void Equipo::vendeAtleta(Atleta& jugador) {
+    jugadores.erase(jugador);
+    jugador.cambiaEquipo("Sin equipo");
+}
+
+void Equipo::contrataEntrenador(Entrenador& trainer) {
+    entrenador = trainer;
+    trainer.cambiaEquipo(getNombre());
+}
+
+void Equipo::despideEntrenador(Entrenador& trainer) {
+    trainer.cambiaEquipo("Sin equipo");
+    entrenador = Entrenador();
+}
+
+#endif // EQUIPO_H_
